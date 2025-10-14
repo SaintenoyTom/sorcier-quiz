ba7f200fc71d958eac5611ad32270b51d75b7ae8
SaintenoyTom <tom.saintenoy@ecoles-epsi.net>
2025-10-14 10:49:15 +0200

    fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,4 @@
 // ESLint v9+ configuration (remplace .eslintrc.json)
-const airbnb = require('eslint-config-airbnb');
-const airbnbHooks = require('eslint-config-airbnb/hooks');
-const tsRecommended = require('@typescript-eslint/eslint-plugin').configs.recommended;
-
 module.exports = [
   {
     ignores: [
@@ -13,28 +9,19 @@ module.exports = [
       'android/',
       'ios/'
     ],
-  },
-  airbnb,
-  airbnbHooks,
-  tsRecommended,
-  {
     plugins: {
       '@typescript-eslint': require('@typescript-eslint/eslint-plugin')
     },
     languageOptions: {
       parser: require('@typescript-eslint/parser'),
-      ecmaVersion: 12,
+      ecmaVersion: 2021,
       sourceType: 'module',
-      globals: {
-        browser: true,
-        es2021: true,
-        jest: true
-      },
       parserOptions: {
         project: './tsconfig.json'
       }
     },
     rules: {
+      // Règles inspirées d'Airbnb, à adapter selon tes besoins
       'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
       'import/extensions': ['error', 'ignorePackages', { ts: 'never', tsx: 'never', js: 'never' }],
       '@typescript-eslint/no-unused-vars': ['error'],
