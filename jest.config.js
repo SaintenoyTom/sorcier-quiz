32009ea1f074ddf2a7522cb85ef9c6606c76b9da
SaintenoyTom <tom.saintenoy@ecoles-epsi.net>
2025-10-14 11:13:38 +0200

    fix test unitaires

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,8 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['lcov', 'text'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(expo-haptics|expo-modules-core)/)'
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 };
