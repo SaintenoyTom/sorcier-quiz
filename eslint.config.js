29cd624e387275f3a149b2fcf165c07cc9e848dc
SaintenoyTom <tom.saintenoy@ecoles-epsi.net>
2025-10-14 10:30:57 +0200

    Fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,43 +1,51 @@
 // ESLint v9+ configuration (remplace .eslintrc.json)
-module.exports = {
-  ignores: [
-    'node_modules/',
-    'build/',
-    'dist/',
-    'coverage/',
-    'android/',
-    'ios/'
-  ],
-  extends: [
-    'airbnb',
-    'airbnb/hooks',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  plugins: ['@typescript-eslint'],
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true
+const airbnb = require('eslint-config-airbnb');
+const airbnbHooks = require('eslint-config-airbnb/hooks');
+const tsRecommended = require('@typescript-eslint/eslint-plugin').configs.recommended;
+
+module.exports = [
+  {
+    ignores: [
+      'node_modules/',
+      'build/',
+      'dist/',
+      'coverage/',
+      'android/',
+      'ios/'
+    ],
   },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-    project: './tsconfig.json'
-  },
-  rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
-    'import/extensions': ['error', 'ignorePackages', { ts: 'never', tsx: 'never', js: 'never' }],
-    '@typescript-eslint/no-unused-vars': ['error'],
-    '@typescript-eslint/explicit-module-boundary-types': 'off'
-  },
-  overrides: [
-    {
-      files: ['**/__tests__/**', '*.spec.*'],
-      rules: {
-        'no-undef': 'off',
-        '@typescript-eslint/no-explicit-any': 'off'
+  ...airbnb,
+  ...airbnbHooks,
+  ...tsRecommended,
+  {
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin')
+    },
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      ecmaVersion: 12,
+      sourceType: 'module',
+      globals: {
+        browser: true,
+        es2021: true,
+        jest: true
+      },
+      parserOptions: {
+        project: './tsconfig.json'
       }
+    },
+    rules: {
+      'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+      'import/extensions': ['error', 'ignorePackages', { ts: 'never', tsx: 'never', js: 'never' }],
+      '@typescript-eslint/no-unused-vars': ['error'],
+      '@typescript-eslint/explicit-module-boundary-types': 'off'
     }
-  ]
-};
+  },
+  {
+    files: ['**/__tests__/**', '*.spec.*'],
+    rules: {
+      'no-undef': 'off',
+      '@typescript-eslint/no-explicit-any': 'off'
+    }
+  }
+];
